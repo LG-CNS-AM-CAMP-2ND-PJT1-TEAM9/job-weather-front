9071904b58dbbe6bb034eda08ddc7b8800ef29da
Ahnhyojun <hyojunahn111@naver.com>
2025-05-19 16:04:16 +0900

    #3 Job_search 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Signup from './pages/signup'
 import Login from './pages/Login'
+import JobSearch from './pages/job_search/job_search.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,8 @@ function App() {
     <Routes>
       <Route path='/users/login' element={<Login/>}/>
       <Route path='/users/signup' element={<Signup/>}/>
+
+      <Route path='/job_search' element={<JobSearch/>}/>
     </Routes>
     </BrowserRouter>
   )
