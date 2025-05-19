1ffa7fc0109925225011967ca3e70ff9414a89b3
Junseo Yoon <58902513+liljuice@users.noreply.github.com>
2025-05-19 17:38:13 +0900

    fix: App.jsx 레이아웃 분기 로직 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import reactLogo from './assets/react.svg'; // 사용하지 않는다면 제거 가능
+// import viteLogo from '/vite.svg'; // 사용하지 않는다면 제거 가능
+import './App.css'; // App.css import
 import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
-import Signup from './pages/signup'
-import Login from './pages/Login'
-import JobSearch from './pages/job_search/job_search.jsx'
-import Resetpw from './pages/Resetpw'
+import Signup from './pages/signup';
+import Login from './pages/Login';
+import JobSearch from './pages/job_search/job_search.jsx'; // JobSearch 페이지 import 추가
+import Resetpw from './pages/Resetpw'; // 비밀번호 재설정 페이지 import
 import MainPage from './pages/MainPage/MainPage';
 
 // 라우트에 따라 다른 레이아웃 클래스를 적용하기 위한 내부 컴포넌트
@@ -19,6 +19,7 @@ function AppLayout() {
     if (location.pathname === '/') { // MainPage 경로
       setLayoutClass('app-layout-main');
     } else {
+      // MainPage 이외의 모든 페이지 (로그인, 회원가입, 비밀번호 재설정, 채용검색 등)
       setLayoutClass('app-layout-default');
     }
   }, [location.pathname]); // 경로가 변경될 때만 이 효과를 다시 실행
@@ -29,6 +30,8 @@ function AppLayout() {
       <Routes>
         <Route path='/users/login' element={<Login/>}/>
         <Route path='/users/signup' element={<Signup/>}/>
+        <Route path='/users/reset-password' element={<Resetpw/>}/> {/* 비밀번호 재설정 라우트 */}
+        <Route path='/job_search' element={<JobSearch/>}/> {/* 채용 검색 페이지 라우트 추가 */}
         <Route path='/' element={<MainPage/>}/>
       </Routes>
     </div>
@@ -38,12 +41,8 @@ function AppLayout() {
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/users/login' element={<Login/>}/>
-        <Route path='/users/signup' element={<Signup/>}/>
-        <Route path='/users/reset-password' element={<Resetpw/>}/>
-        <Route path='/job_search' element={<JobSearch/>}/>
-      </Routes>
+      {/* AppLayout 컴포넌트를 여기서 사용해야 합니다. */}
+      <AppLayout />
     </BrowserRouter>
   );
 }
